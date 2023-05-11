551a9cc774ac318f41ce14ddd99b81b00d531a58
Luis <luis.ricardo@tech4h.com.br>
2023-05-11 14:44:44 -0300

    eslint

--- bank-charge/src/database/dao/monsters.js
+++ bank-charge/src/database/dao/monsters.js
@@ -6,14 +6,14 @@ const saveMonster = async (state, monster) => {
   try {
     writeLog("\tCriando models do monstro");
 
-    
-
     await transaction.commit();
     writeLog("\tSalvo no banco de dados".green);
   } catch (error) {
     writeLog(`\t[Erro] ${error.message}`.red);
     writeLog(`\t[Erro] : SQL: ${JSON.stringify(error.sql, 0, 2)}`.red);
-    writeLog(`\t[Erro] : Params: ${JSON.stringify(error.parameters, 0, 2)}`.red);
+    writeLog(
+      `\t[Erro] : Params: ${JSON.stringify(error.parameters, 0, 2)}`.red
+    );
     await transaction.rollback();
 
     throw new Error(
