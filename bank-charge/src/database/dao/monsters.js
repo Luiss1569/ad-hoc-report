e9a360052fbb1a355cc1ec700a46271549a0d1eb
Luis <luis.ricardo@tech4h.com.br>
2023-05-11 14:42:30 -0300

    feat: add dao

--- bank-charge/src/database/dao/monsters.js
+++ bank-charge/src/database/dao/monsters.js
@@ -1,9 +1,27 @@
 const saveMonster = async (state, monster) => {
-    const { conn, writeLog } = state;
+  const { conn, writeLog } = state;
+  writeLog("\tInciando Transacao no banco de dados");
+  const transaction = await conn.transaction();
 
-    writeLog("\tSalvando monstro no banco");
+  try {
+    writeLog("\tCriando models do monstro");
+
+    
+
+    await transaction.commit();
+    writeLog("\tSalvo no banco de dados".green);
+  } catch (error) {
+    writeLog(`\t[Erro] ${error.message}`.red);
+    writeLog(`\t[Erro] : SQL: ${JSON.stringify(error.sql, 0, 2)}`.red);
+    writeLog(`\t[Erro] : Params: ${JSON.stringify(error.parameters, 0, 2)}`.red);
+    await transaction.rollback();
+
+    throw new Error(
+      `[Erro] Salvar o monstro ${monster.slug} no banco de dados`
+    );
+  }
 };
 
 module.exports = {
-    saveMonster,
+  saveMonster,
 };
