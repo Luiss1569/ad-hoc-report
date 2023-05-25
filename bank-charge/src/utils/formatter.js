3f2ab7afe1e6a27714331dce334142177c4f6d1e
Augusto Benteu <benteu82@gmail.com>
2023-05-25 18:12:48 -0300

    formatação ?

--- bank-charge/src/utils/formatter.js
+++ bank-charge/src/utils/formatter.js
@@ -1,6 +1,31 @@
 const formatterMonster = (writeLog, monster) => {
+  monsterCollums=['name','size','type','subtype','group', 'groupp','strength','constitution','dexterity',
+  'intelligence','wisdom','charisma','strength_save','dexterity_save','constitution_save','intelligence_save',
+  'wisdom_save','charisma_save','perception','damage_vulnerabilities','damage_resistances','damage_immunities',
+  'condition_immunities','senses','languages','challenge_rating','legendary_desc','legendary_actions','page_no',
+  'img_main', 'slug','speed','skill','special_abilities','spells']
   writeLog("\tFormatando monstro");
-
+  delete monster.document__slug
+  delete monster.document__title
+  delete monster.document__license_url
+  delete monster.document__url
+  for(var action of monster.actions){
+    action.description = action.desc;
+    delete action.desc;
+  }
+  for(var specialAbility of monster.special_abilities){
+    specialAbility.description = specialAbility.desc;
+    delete specialAbility.desc;
+  }
+  for(const key in monster){
+    if(!monsterCollums.contains(key)){
+      delete monster[key]
+    }
+    if(key == 'group'){
+      monster.groupp = monster[key]
+      delete monster[key]
+    }
+  }
   return monster;
 };
 
